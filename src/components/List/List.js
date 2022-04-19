b81d2953302b8ce5b97bbf7a594f90ac58c7c11e
Krzysztof Potocki <kpotocki89@gmail.com>
2022-04-19 21:50:45 +0200

    Add local styles for Hero, List and SearchForm

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,23 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
-    <div>
-      <h1>Things to do <span>soon</span></h1>
-      <p>Interesting things I want to check out</p>
-      <div>
-      <article>
-        <h1>Books</h1>
-      </article>
-      <article>
-        <h1>Movies</h1>
-      </article>
-      <article>
-        <h1>Games</h1>
-      </article>
-      </div>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Things to do<span>soon</span></h1>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        <article>
+          <h2>Books</h2>
+        </article>
+        <article>
+          <h2>Movies</h2>
+        </article>
+        <article>
+          <h2>Games</h2>
+        </article>
+      </section>
     </div>
   );
 };
