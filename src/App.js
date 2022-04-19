f58e2194de20349f0ff58af9d43f238709e7a660
Krzysztof Potocki <kpotocki89@gmail.com>
2022-04-19 20:32:21 +0200

    Add global styles, add list function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
+import List from './components/List/List';
+import SearchForm from './components/SearchForm/SearchForm';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 const App = () => {
   return (
     <div>
       <Hero />
       <SearchForm />
+      <List />
     </div>
   );
 };
---
+++ src/components/List/List.js
@@ -0,0 +1,21 @@
+const List = () => {
+  return (
+    <div>
+      <h1>Things to do <span>soon</span></h1>
+      <p>Interesting things I want to check out</p>
+      <div>
+      <article>
+        <h1>Books</h1>
+      </article>
+      <article>
+        <h1>Movies</h1>
+      </article>
+      <article>
+        <h1>Games</h1>
+      </article>
+      </div>
+    </div>
+  );
+};
+
+export default List;
