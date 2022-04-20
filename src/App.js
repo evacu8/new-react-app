79277b10a41b9937ea26192e207337d6db694904
Krzysztof Potocki <kpotocki89@gmail.com>
2022-04-20 21:09:46 +0200

    Change structure - add container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Hero from './components/Hero/Hero'
 import List from './components/List/List';
 import SearchForm from './components/SearchForm/SearchForm';
+import Container from './components/Container/Container';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
 const App = () => {
   return (
     <div>
-      <Hero />
-      <SearchForm />
-      <List />
+      <Container>
+        <Hero />
+        <SearchForm />
+        <List />
+      </Container>
     </div>
   );
 };
