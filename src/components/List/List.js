616504671128b5bd3a89a2277ab7de325312faf0
Krzysztof Potocki <kpotocki89@gmail.com>
2022-05-15 21:37:37 +0200

    Add Redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,6 @@
 import styles from './List.module.scss';
 import Column from './../Column/Column';
 import ColumnForm from './../ColumnForm/ColumnForm';
-import CardForm from '../CardForm/CardForm';
 import { useState } from 'react';
 import shortid from 'shortid';
 
