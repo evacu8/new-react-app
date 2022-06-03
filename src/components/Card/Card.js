80be8e24af31f4c3ace33e93b784647105fb86a9
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-03 10:45:39 +0200

    Add Favorite functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,30 @@
 import styles from './Card.module.scss';
+import clsx from 'clsx';
+import { useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavorite } from '../../redux/store';
 
 const Card = props => {
+
+  const dispatch = useDispatch();
+  const [isFavorite, setIsFavorite] = useState(props.isFavorite);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsFavorite(!isFavorite);
+    dispatch(toggleCardFavorite(props.id));
+  }
+
   return (
-    <li className={styles.card}>{props.title}</li>
+    <li className={styles.card}>
+      {props.title}
+      <button 
+      type='button'
+        className={clsx(styles.button, isFavorite && styles.favorite)} 
+        onClick={handleClick}>
+        <span className="fa fa-star-o" />
+      </button>
+    </li>
   );
 };
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,24 @@
-import styles from './Favorite.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
+import styles from './Favorite.module.scss';
+import Card from '../Card/Card';
+
+import { getFavorites } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const Favorite = () => {
+  const favoriteCards = useSelector(state => getFavorites(state));
+
+  if (favoriteCards.length === 0) return <Navigate to='/' />;
+
   return (
-    <div className={styles.favorite}>
+    <div>
       <PageTitle>Favorite</PageTitle>
-      <p className={styles.subtitle}>Lorem ipsum</p>
+      <ul className={styles.column}>
+        {favoriteCards.map((card) => (
+          <Card key={card.id} title={card.title} id={card.id} isFavorite={card.isFavorite} columnId={card.columnId} />
+        ))}
+      </ul>
     </div>
   );
 };
