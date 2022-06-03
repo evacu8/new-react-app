5d2178325c396fc7bc14ff6231c29e866ac1424a
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-03 17:56:38 +0200

    Add 'remove card' functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,28 +2,41 @@ import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useState} from 'react';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
 
   const dispatch = useDispatch();
   const [isFavorite, setIsFavorite] = useState(props.isFavorite);
 
-  const handleClick = (e) => {
+  const handleFavorite = (e) => {
     e.preventDefault();
     setIsFavorite(!isFavorite);
     dispatch(toggleCardFavorite(props.id));
+  }
+  
+  const handleRemove = (e) => {
+    e.preventDefault();
+    dispatch(removeCard(props.id));
   }
 
   return (
     <li className={styles.card}>
       {props.title}
-      <button 
-        type='button'
-        className={clsx(styles.button, isFavorite && styles.favorite)} 
-        onClick={handleClick}>
-        <span className="fa fa-star-o" />
-      </button>
+      <div>
+        <button 
+          type='button'
+          className={clsx(styles.button, isFavorite && styles.favorite)} 
+          onClick={handleFavorite}>
+          <span className="fa fa-star-o" />
+        </button>
+        <button 
+          type='button'
+          className={styles.button} 
+          onClick={handleRemove}>
+          <span className="fa fa-trash" />
+        </button>
+      </div>
     </li>
   );
 };
